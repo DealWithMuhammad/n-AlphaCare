112ccd6daf323b50d8b131917e97c194278c005d
Muhammad Ahmad <muhammad.ahmad90@icloud.com>
2025-02-06 17:47:05 +0800

    feat: add home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,40 @@
+import PatientForm from "@/components/forms/PatientForm";
+import { Link } from "lucide-react";
+import Image from "next/image";
+
 export default function Home() {
   return (
     <div className="flex h-screen max-h-screen">
       <section className="container remove-scrollbar my-auto">
-        <div className="sub-container max-w-[496px]"></div>
+        <div className="sub-container max-w-[496px]">
+          <div className="flex space-x-2 items-center mb-12">
+            <Image
+              src="/assets/icons/logo-icon.svg"
+              height={1000}
+              width={1000}
+              alt="patient"
+              className=" h-10 w-fit "
+            />
+            <h1 className="text-2xl font-bold"> AlphaCare</h1>
+          </div>
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © All rights reserved by Muhammad | 2025
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
+        </div>
       </section>
+      <Image
+        src="/assets/images/onboarding-img.png"
+        height={1000}
+        width={1000}
+        alt="patient"
+        className="side-img max-w-[50%]"
+      />
     </div>
   );
 }
